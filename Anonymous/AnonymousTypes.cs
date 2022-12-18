5df780bfba00ffce45204692bc79cb3f818e345c
me_shytka <me_shytka@mail.ru>
2022-12-18 19:09:12 +0300

    Add Anonymous project

---
+++ Anonymous/AnonymousTypes.cs
@@ -0,0 +1,31 @@
+﻿namespace TestSolutions
+{
+    public class AnonymousTypes
+    {
+        public void Sample()
+        {
+            // anonymous class are generic
+            // When anonymous objects are created it also generated right methods:
+            // ToString
+            // Equals (that compares objects not by reference, but by value)
+            // GetHashCode (so we can use them as a key in a Dictionary and with Linq.GroupBy)
+            var anonymType = new { Hello = "Hello", World = "World" };
+
+            var dict = Enumerable.ToDictionary(new List<string>(), key => new { Hello = "Hello1", World = "World1" }, value => "");
+            dict.Add(anonymType, "");
+
+            var anonymType2 = new { Hello = "Hello", World = "World" };
+            dict.Add(anonymType2, "");
+
+            var anonymType3 = new { Hello = "Hello", World = "World" };
+            var result = dict.Select(el => el.Key == anonymType3);
+
+            // This will be the same anonymous class as the previous (generic, but with different types AnonymousType<string,string> vs AnonymousType<int,List<bool>)
+            // it saves memory
+            var anonymType4 = new { Hello = 123, World = new List<bool>() };
+
+            // Different anonymous class because different field names
+            var anonymType5 = new { Hello1 = "Hello", World2 = "World" };
+        }
+    }
+}

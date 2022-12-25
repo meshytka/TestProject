b2c4596063a5317d548321c58218ad3d91e45830
me_shytka <me_shytka@mail.ru>
2022-12-25 13:32:02 +0300

    Add Events

---
+++ Events/ClassWithEvents.cs
@@ -0,0 +1,25 @@
+﻿namespace Events
+{
+    public class SomeClass
+    {
+        public void NewMethod() 
+        {
+            var evt = new ClassWithEvents();
+            evt.OnEvent += Foo;
+            evt.OnEvent += () =>
+            {
+                Foo();
+                Console.WriteLine("");
+            };
+        }
+
+        public void Foo() { }
+    }
+
+    public class ClassWithEvents
+    {
+        // Events based on delegates
+        // Events are thread-safe
+        public event Action OnEvent;
+    }
+}
